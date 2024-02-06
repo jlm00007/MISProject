d69498f4f50a30df79c4e602ed43c2a86c54a260
MISTUser <MISTUser@lab8L4JUY>
2024-02-05 19:29:47 -0500

    Did Everything

---
+++ MISProject/Pages/Mets.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MISProject.Pages
+{
+    public class MetsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
