9428ad3a18b32ddfd4b512ec09c1a5a0a492b243
MISTUser <MISTUser@lab8L4JUY>
2024-03-19 12:51:53 -0400

    Pushing Team Entity

---
+++ BaseballSiteAPI/Entities/Team.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseballSiteAPI.Entities
+{
+    public class Team
+    {
+        public short TID { get; set; }
+        public required string TName { get; set; }
+        public required string City { get; set; }
+        public required string State { get; set; }
+        public required string League { get; set; }
+        public required string Division { get; set; }
+    }
+}
