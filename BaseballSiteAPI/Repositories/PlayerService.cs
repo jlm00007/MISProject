f29fa990395b0d14289842fd83513bfe5f3bb510
dustinnull <150753989+dustinnull@users.noreply.github.com>
2024-04-04 13:04:07 -0400

    Update PlayerService.cs
    Added "PlayerGetDetails" and "AddPlayer"

--- BaseballSiteAPI/Repositories/PlayerService.cs
+++ BaseballSiteAPI/Repositories/PlayerService.cs
@@ -25,5 +25,26 @@ namespace BaseballSiteAPI.Repositories
             return PlayerPos;
         }
 
+        public async Task<List<Player>> PlayerGetDetails(int PID)
+        {
+
+            var param = new SqlParameter("@PID", PID);
+            var playerDetails = await Task.Run(() => _dbContextClass.Player.FromSqlRaw("exec spGetPlayerDetails @PID", param).ToListAsync());
+            return playerDetails;
+
+        }
+        public async Task<int> AddPlayer(Player player)
+        {
+
+            var PID = new SqlParameter("@PID", player.PID);
+            var pName = new SqlParameter("Name", player.Name);
+            var pNumber = new SqlParameter("Number", player.Number);
+            var pPosition = new SqlParameter("Position", player.Position);
+            var TID = new SqlParameter("TID", player.TID);
+            var playerDetails = await Task.Run(() => _dbContextClass.Database.ExecuteSqlRaw("exec spAddPlayer @PID SMALLINT,\r\n    @Name NVARCHAR(max),\r\n    @Number NVARCHAR(max),\r\n    @Position NVARCHAR(max), \r\n    @TID SMALLINT"));
+            return playerDetails;
+
+        }
+
     }
 }
